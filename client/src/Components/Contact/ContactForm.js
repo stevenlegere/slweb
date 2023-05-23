78cdc6fa02db68ecd1353cf9004dca5cf2b8c42a
stevenlegere <stevenmarklegere@gmail.com>
2023-05-23 21:48:24 +0100

    Domain successful and working on contact page

---
+++ client/src/Components/Contact/ContactForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import MyAppBar from '../MyAppBar/MyAppBar';
+
+
+const ContactForm = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post('/api/send-email', formData);
+            alert('Email sent!');
+            setFormData({ name: '', email: '', message: '' });
+        } catch (error) {
+            console.error('Error sending email: ', error);
+            alert('Error sending email');
+        }
+        };
+
+        return (
+            <div>
+                <MyAppBar />
+            
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                    />
+                <textarea
+                    name="message"
+                    placeholder="Message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    required
+                    ></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+
+        );
+    };
+   export default ContactForm;
--- server/index.js
+++ server/index.js
@@ -1,24 +1,67 @@
 // Write some code that will tell the server to disply react app when user goes to home page
-const path = require('path');
-const express = require('express');
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/api/send-email", (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Create a transporter using your Gmail account credentials
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USERNAME,
+      pass: process.env.GMAIL_PASSWORD,
+    },
+  });
+
+  // Configure the email details
+  const mailOptions = {
+    from: "your-gmail-account@gmail.com",
+    to: "your-gmail-account@gmail.com",
+    subject: "New Contact Form Submission",
+    text: `
+        Name: ${name}
+        Email: ${email}
+        Message: ${message}
+      `,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while sending the email." });
+    } else {
+      console.log("Email sent:", info.response);
+      res.status(200).json({ message: "Email sent successfully!" });
+    }
+  });
+});
+
 // Have Node serve the files for built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // If React App makes GET request to the route, respond with a res with our JSON data.
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from server!" });
 });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 // This code will first allow Node to access our built React project using the express.static function for static files
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
-
