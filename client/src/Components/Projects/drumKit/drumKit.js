1d1e76a3613c7cf9636d7943e7d05ea2640748ff
stevenlegere <stevenmarklegere@gmail.com>
2023-06-03 12:46:31 +0100

    added mobile fucntionality

--- client/src/Components/Projects/drumKit/drumKit.js
+++ client/src/Components/Projects/drumKit/drumKit.js
@@ -13,31 +13,68 @@ import tinkSound from "./sounds/tink.wav";
 
 function DrumKit() {
   useEffect(() => {
-    function playSound(e) {
-      const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-      const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    function playSound(keyCode) {
+      const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+      const key = document.querySelector(`.key[data-key="${keyCode}"]`);
       if (!audio) return;
       audio.currentTime = 0;
-      audio.play();
-      key.classList.add("playing");
+      
+      // Check if the audio can be played
+      const playPromise = audio.play();
+      if (playPromise !== undefined) {
+        playPromise
+          .then(() => {
+            // Audio playback started successfully
+            key.classList.add("playing");
+          })
+          .catch((error) => {
+            // Error occurred while starting audio playback
+            console.error("Failed to play audio:", error);
+          });
+      }
     }
 
     function removeTransition(e) {
       if (e.propertyName !== "transform") return;
-      this.classList.remove("playing");
+      e.target.classList.remove("playing");
+    }
+
+    function handleKeyDown(e) {
+      playSound(e.keyCode);
+    }
+
+    function handleTouchStart(e) {
+      const keyCode = e.currentTarget.dataset.key;
+      playSound(keyCode);
+    }
+
+    function handleTouchEnd(e) {
+      const keyCode = e.currentTarget.dataset.key;
+      const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+      if (key) {
+        key.classList.remove("playing");
+      }
     }
 
     const keys = document.querySelectorAll(".key");
     keys.forEach((key) =>
       key.addEventListener("transitionend", removeTransition)
     );
-    window.addEventListener("keydown", playSound);
+    window.addEventListener("keydown", handleKeyDown);
+    keys.forEach((key) => {
+      key.addEventListener("touchstart", handleTouchStart);
+      key.addEventListener("touchend", handleTouchEnd);
+    });
 
     return () => {
       keys.forEach((key) =>
         key.removeEventListener("transitionend", removeTransition)
       );
-      window.removeEventListener("keydown", playSound);
+      window.removeEventListener("keydown", handleKeyDown);
+      keys.forEach((key) => {
+        key.removeEventListener("touchstart", handleTouchStart);
+        key.removeEventListener("touchend", handleTouchEnd);
+      });
     };
   }, []);
 
@@ -86,14 +123,33 @@ function DrumKit() {
         </div>
       </div>
       <div className="drumKitText">
-      <p>
-        In my latest personal project I wanted to practie by working through a <a href="https://javascript30.com/" style={{color: '#ff6d80'}}>JavaScript 30</a> tutorial by drilling home some great Vanilla JavaScript, and then re-factor into React, bonus is I now have two code sets I can look back on at anytime.
-
-        To begin, I followed the JavaScript 30 tutorial and completed the project using vanilla JavaScript and HTML. This allowed me to understand the core functionality and logic behind the project. Once I had a solid grasp of the original implementation, I decided to take it a step further by converting it to a React application.
-
-        First, I created a new React component called DrumKit, to encapsulate the entire project. I transferred the relevant HTML code from the tutorial into the render method of the DrumKit component, ensuring that all the necessary elements and classes were included.
-
-        Next, I use useEffect hook to handle the event listeners and sound playback. I translated the original JavaScript functions, such as playSound and removeTransition, into their respective useEffect callbacks. This allowed me to listen for keyboard events and trigger the corresponding sound playback within the React component.
+        <p>
+          In my latest personal project, I wanted to practice by working through a{" "}
+          <a href="https://javascript30.com/" style={{ color: "#ff6d80" }}>
+            JavaScript 30
+          </a>{" "}
+          tutorial by drilling home some great Vanilla JavaScript, and then re-factor
+          into React. The bonus is I now have two code sets I can look back on at any time.
+        </p>
+        <p>
+          To begin, I followed the JavaScript 30 tutorial and completed the project
+          using vanilla JavaScript and HTML. This allowed me to understand the core
+          functionality and logic behind the project. Once I had a solid grasp of the
+          original implementation, I decided to take it a step further by converting it
+          to a React application.
+        </p>
+        <p>
+          First, I created a new React component called DrumKit to encapsulate the
+          entire project. I transferred the relevant HTML code from the tutorial into the
+          render method of the DrumKit component, ensuring that all the necessary
+          elements and classes were included.
+        </p>
+        <p>
+          Next, I used the useEffect hook to handle the event listeners and sound
+          playback. I translated the original JavaScript functions, such as playSound
+          and removeTransition, into their respective useEffect callbacks. This allowed
+          me to listen for keyboard and touch events and trigger the corresponding sound playback
+          within the React component.
         </p>
       </div>
 
