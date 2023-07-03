c835f7e0c23c36709f1c4c12745ab1f27a4880c0
stevenlegere <stevenmarklegere@gmail.com>
2023-07-03 19:10:51 +0100

    Testing repo for new vs Code setup

--- client/src/Components/Projects/projects.js
+++ client/src/Components/Projects/projects.js
@@ -17,7 +17,7 @@ function Projects() {
             <br></br>
                 <Link to ="https://littr.netlify.app/" style={{color: '#ff6d80'}} target="_blank">LITTR</Link><p style={{ color: '#2497ea', }}> School of Code final project - 5 weeks to build a web App that would address a real-world issue.</p>
                 <h3 style={{ color: '#2497ea', }}>Problem Statement:</h3><p style={{ color: '#2497ea', }}> Our environment is overrun with litter - by encouraging local communication and pride, we aim to help communities struggling to reclaim their green-spaces, beaches and neighbourhoods.</p>
-                <p style={{ color: '#2497ea', }}>Tech Stack: React / Supabase / Netlify / React Testing Library / HTML / CSS / MUI / Figma / Trello</p>
+                <p style={{ color: '#2497ea', }}>Tech Stack: React / Supabase / Netlify / React Testing Library / HTML / CSS / MUI / Figma / Trello.</p>
                 <br></br>
                 <br></br>
                 <Link to ="./Drumkit" style={{color: '#ff6d80'}}>JavaScript Drum Kit</Link>
